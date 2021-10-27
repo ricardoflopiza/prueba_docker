72a7b4ae1cdfce46141be811659a240576c70078
Ricardo <rik990@gmail.com>
2021-10-27 16:29:07 -0300

    prueba weona

--- R/app.R
+++ R/app.R
@@ -2,9 +2,6 @@
 
 ## Pendientes ####
 
-# La base de la ENUT se descarga 
- 
- 
 ### carga de paquetes ####
 library(shinyFeedback)
 library(calidad)
@@ -27,6 +24,7 @@ library(writexl)
 library(shinyjs)
 library(tibble)
 library(plotly)
+library(feather)
 
 # Cargar funciones ####
 #source("download_data.R")
@@ -105,7 +103,7 @@ Esta aplicación permite implementar el estándar de calidad para las estimacion
                                      status = "primary",
                                      justified = TRUE
                                    ),
-                                   h5("En esta Sección puedes seleccionar la opción de cargar una base de datos desde tu computador, o cargar una base de datos del INE"),
+                                   h5("EnPRIBANDO EFSASGDGDGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGo cargar una base de datos del INE"),
                                    uiOutput("datos_locales"),
                                    uiOutput("DescargaINE"),
                                    ## render selección de variables de interes, y de cruce
